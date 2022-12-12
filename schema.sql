190b1489d6d1505f0a83978b6fe11c184d653b95
taldr27 <diegogarciaq98@gmail.com>
2022-12-12 13:15:48 -0500

    Add primary and generate id automatically

--- schema.sql
+++ schema.sql
@@ -1,10 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    ID INTEGER,
-    NAME VARCHAR,
-    DATE_OF_BIRTH DATE,
-    ESCAPE_ATTEMPTS INTEGER,
-    NEUTERED BOOLEAN,
-    WEIGHT_KG DECIMAL
+    id INTEGER GENERATED ALWAYS AS IDENTITY NOT NULL PRIMARY KEY,
+    name VARCHAR,
+    date_of_birth DATE,
+    escape_attempts INTEGER,
+    neutered BOOLEAN,
+    weight_kg DECIMAL
 );
