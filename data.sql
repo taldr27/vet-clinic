ea3cc3d988dfdaf081fdb6c646565fe4c4502ca1
taldr27 <diegogarciaq98@gmail.com>
2022-12-14 11:14:55 -0500

    Add queries

--- data.sql
+++ data.sql
@@ -28,3 +28,16 @@ INSERT INTO owners(full_name, age) VALUES ('Jodie Whittaker', 38);
 
 INSERT INTO species(name) VALUES ('Pokemon');
 INSERT INTO species(name) VALUES ('Digimon');
+
+/* Insert species */
+
+UPDATE animals SET species_id=2 where name LIKE '%mon';
+UPDATE animals SET species_id=1 WHERE species_id IS null;
+
+/* Insert owners */
+
+UPDATE animals SET owner_id=1  where name= 'Agumon';
+UPDATE animals SET owner_id=2  where name='Gabumon' OR name='Pikachu';
+UPDATE animals SET owner_id=3  where name='Devimon' OR name='Plantmon';
+UPDATE animals SET owner_id=4  where name='Charmander' OR name='Squirtle' OR name='Blossom';
+UPDATE animals SET owner_id=5  where name='Angemon' OR name='Boarmon';
